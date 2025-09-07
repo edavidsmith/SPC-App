aabcab5e537e7859fe3220704003e76585e0d233
Elias Smith <eliasdavidsmith52@gmail.com>
2025-09-07 13:25:01 -0500

    Add bad input handling, improve readability

--- SPC-App.py
+++ SPC-App.py
@@ -17,10 +17,10 @@ def CityToCoord(city):
     lat = json[0]['lat']
     lon = json[0]['lon']
 
-    return {"latitude": lat, "longitude": lon}
+    return {"latitude": lat, "longitude": lon, "city-name": city}
 
 def DownloadZipFile(desired_zipfilename):
-    #STEP 2: a zip file's url is located, utilizing knowledge of the SPC's predictable naming conventions
+    #STEP 2: a zip file for download is located, utilizing knowledge of the SPC's predictable URL naming conventions
     day1_zulu = (600,1300,1630,2000,100)
     time = datetime.now(timezone.utc)
     time_replace = time.isoformat(timespec='milliseconds').replace('+00:00', 'Z')
@@ -36,7 +36,7 @@ def DownloadZipFile(desired_zipfilename):
     full_url = f"https://www.spc.noaa.gov/products/outlook/archive/{today_date[:4]}/day1otlk_{today_date}_{current_day1_outlook_time:04d}-shp.zip"
     source_zip_file = requests.get(full_url)
 
-    #uses previous zulu time's outlook if the SPC has not updated the page yet
+    #if current_day1_outlook_time is not up to date yet, uses previous zulu time
     if source_zip_file.status_code != 200:
         fixed_outlook_index = day1_zulu.index(current_day1_outlook_time) - 1
         full_url = f"https://www.spc.noaa.gov/products/outlook/archive/{today_date[:4]}/day1otlk_{today_date}_{day1_zulu[fixed_outlook_index]:04d}-shp.zip"
@@ -47,12 +47,13 @@ def DownloadZipFile(desired_zipfilename):
     
 
 def ZipFileIteration(zipfilename, user_specified_outlook):
+    #STEP 3: iterate over contents of downloaded .zip file, making sure to only extract what is necessary based on user's specification
     with ZipFile(zipfilename,'r') as myzip:
         file_list = myzip.namelist()
 
         #only necessary files are extracted based on desired outlook
         for i in file_list:
-            if user_specified_outlook == "cat" and "cat" in i:
+            if user_specified_outlook == "categorical" and "cat" in i:
                 if "shp" in i:
                     name_of_desired = i
                 myzip.extract(i)
@@ -60,7 +61,7 @@ def ZipFileIteration(zipfilename, user_specified_outlook):
                 if "shp" in i:
                     name_of_desired = i
                 myzip.extract(i)
-            if user_specified_outlook == "tor" and not "sig" in i and "torn" in i:
+            if user_specified_outlook == "tornado" and not "sig" in i and "torn" in i:
                 if "shp" in i:
                     name_of_desired = i
                 myzip.extract(i)
@@ -71,18 +72,23 @@ def ZipFileIteration(zipfilename, user_specified_outlook):
 
         return name_of_desired
 
-def ShapeFileComparison():
+def ShapeFileParsed():
+    #STEP 4: the .shp file extracted from earlier is parsed and used to tell the user what risk area their specified city is in
     city = CityToCoord(input("Enter a city: "))
-    user_query_which_outlook = input("Which outlook do you wish to view? (cat,tor,hail,wind): ")
+    user_query_which_outlook = input("Which type of outlook do you wish to view? (categorical,tornado,hail,wind): ").lower().strip()
+    
+    #loop handles bad input
+    accepted_input = ("categorical","tornado","hail","wind")
+    while user_query_which_outlook not in accepted_input:
+        user_query_which_outlook = input("Input not accepted. Which type of outlook do you wish to view? (categorical,tornado,hail,wind): ").lower().strip()
+
     zip_file_name = "spcdata.zip"
     DownloadZipFile(zip_file_name)
     name_of_file = ZipFileIteration(zip_file_name,user_query_which_outlook)
 
     shape_file = gpd.read_file(name_of_file) 
-    
     shape_dict = shape_file.to_geo_dict()
-
-    gdf = gpd.GeoDataFrame.from_features(shape_dict["features"])
+    gdf = gpd.GeoDataFrame.from_features(shape_dict["features"]) #the features (ie coordinates) from the extacted shp file are accessed
 
     coord_to_use = gpd.GeoSeries([Point(city["longitude"],city["latitude"])], crs="EPSG:3857")
     gdf.set_crs("EPSG:3857", inplace=True)
@@ -94,14 +100,13 @@ def ShapeFileComparison():
             risk_exists = True
 
     if not risk_exists:
-        return "No storm risks today" 
+        print("No storm risks today")
     else:
-        risk_name = gdf.loc[num_caught,"LABEL2"] #based on the number label that evaluated "True" for .contains(), its corresponding risk label is accessed thus
-        return risk_name #this returns a string
+        risk_name = gdf.loc[num_caught,"LABEL2"] #based on the Integer label that evaluated "True" for .contains(), its corresponding String risk label is accessed thus
+        print(f"For today's {user_query_which_outlook} convective outlook in {city["city-name"]}, there is a {risk_name} in place") #this returns a string
 
 def main():
-    print(ShapeFileComparison())
-
+    ShapeFileParsed()
     protected_files = ["SPC-App.py", "README.md", ".gitignore"]
 
     for i in os.listdir():
